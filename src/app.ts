7c0bf1d8f6ec999325893b56550f6915193ee782
Jcroxford <takeflightsc@gmail.com>
2018-12-19 19:35:45 -0700

    added basic logging to app and sn-agenda route scafforld

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 require('dotenv').config()
 
 import express from 'express'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
+import fs from 'fs'
 
 const app = express()
 
-app.get('/ping', (_, res) => {
-  res.send('pong')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+if (process.env.NODE_ENV === 'development') app.use(logger('dev'))
+
+app.post('/sn-agenda', (req, res) => {
+  fs.writeFileSync('res.json', JSON.stringify(req.body, null, 2))
+  res.send('its working!')
 })
 
 app.listen(process.env.PORT, () => console.log(`server is running on http://localhost:${process.env.PORT}`))

1129ca56daf496d89a444fa8d2d2f7087d6fb7fb
jesse croxford <takeflightsc@gmail.com>
2019-01-13 23:40:38 -0700

    added channel info retrieval method

--- src/utils/slack.api.ts
+++ src/utils/slack.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // types
 import { HttpHeaders } from './../types/axios.types'
-import { UserInfo } from '../types/slackResponse.types'
+import { UserInfo, ChannelInfo } from '../types/slackResponse.types'
 
 function http(json: boolean = true) {
   const headers: HttpHeaders = {
@@ -11,6 +11,7 @@ function http(json: boolean = true) {
 
   if (!json) headers['Content-Type'] = 'application/x-www-form-urlencoded'
   return axios.create({
+    baseURL: 'https://slack.com/api',
     headers: {
       Authorization: `Bearer ${process.env.SLACK_BOT_AUTH}`,
     },
@@ -21,8 +22,16 @@ export default {
   users: {
     info(user: string): Promise<{ data: UserInfo }> {
       return http(/* json */ false)
-        .get('https://slack.com/api/users.info', {
+        .get('/users.info', {
           params: { user, token: process.env.SLACK_API_TOKEN }
+        })
+    }
+  },
+  channels: {
+    info(channel: string): Promise<{ data: ChannelInfo }> {
+      return http(/* json */ false)
+        .get('/channels.info', {
+          params: { channel, token: process.env.SLACK_API_TOKEN }
         })
     }
   }

c12533e26661787c1046bc2345b61b0cb1291edf
jesse croxford <takeflightsc@gmail.com>
2019-01-13 22:44:36 -0700

    updated knex config filepath in custom connection file

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -1,4 +1,4 @@
 const env = process.env.NODE_ENV || 'development'
-const config = require('../knexfile')[env]
+const config = require('../../knexfile')[env]
 
 export default require('knex')(config)

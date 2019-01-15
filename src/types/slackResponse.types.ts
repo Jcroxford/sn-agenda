111b2c9fc52eb8a46599b951f371ade78cb93a4d
jesse croxford <takeflightsc@gmail.com>
2019-01-14 18:18:56 -0700

    made slack attachment type properties optional

--- src/types/slackResponse.types.ts
+++ src/types/slackResponse.types.ts
@@ -107,24 +107,25 @@ export interface ChannelInfo {
   }
 }
 
+// expected type for sending message to slack
 export interface Attachment {
-  fallback: string
-  color: string
-  pretext: string
-  author_name: string
-  author_link: string
-  author_icon: string
-  title: string
-  title_link: string
-  text: string
-  fields: [{
-    title: string
-    value: string
-    short: boolean
+  fallback?: string
+  color?: string
+  pretext?: string
+  author_name?: string
+  author_link?: string
+  author_icon?: string
+  title?: string
+  title_link?: string
+  text?: string
+  fields?: [{
+    title?: string
+    value?: string
+    short?: boolean
   }]
-  image_url: string
-  thumb_url: string
-  footer: string
-  footer_icon: string
-  ts: number
+  image_url?: string
+  thumb_url?: string
+  footer?: string
+  footer_icon?: string
+  ts?: number
 }
